918b227b7070b61b6c00dc83da97849627a07fc3
fraxion <fraxion26@gmail.com>
2025-10-10 16:20:53 +0200

    fix react version

--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -1,8 +1,11 @@
 // page d'accueil / homepage
 
+import { Carousel } from "../../ui/carousel/Carousel";
+
 export const Homepage = () => {
     return(
         <div>
+            <Carousel/>
 
         </div>
     );
