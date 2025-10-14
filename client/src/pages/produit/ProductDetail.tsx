6f19300b5a03d85b936f29ce73b0131519e56ab9
estelle <haubois.estelle@gmail.com>
2025-10-14 14:12:32 +0200

    add product page

--- client/src/pages/produit/ProductDetail.tsx
+++ client/src/pages/produit/ProductDetail.tsx
@@ -1,10 +1,40 @@
-// page de détail d'un produit 
-//  affiche le nopm du produit, le prix, etc.
+// import { useParams, useNavigate } from 'react-router-dom';
+import { useProductsFetch } from '../../hooks/useProductsFetch';
+import { Product } from '../../models/Products';
+import { ImgComp } from '../../ui/imgComp/ImgComp';
+import './productDetail.css';
+
 
 export const ProductDetail = () => {
-    return(
-        <div>
+  const products: Product[] = useProductsFetch({ target: 3 });
+  const product = products[0];
 
+  if (!product) {
+    return <div className="loading-container">Erreur lors du chargement du produit...</div>;
+  }
+
+  return (
+    <div className="product-detail-page">
+      <div className="product-detail-container">
+        <div className="product-image-container">
+          <ImgComp path={product.picturePath} size={300}/>
         </div>
-    );
-}
+
+        <div className="product-info">
+          <h1 className="product-name">{product.name}</h1>
+
+          <p className="product-price">{product.price.toFixed(2)} €</p>
+
+          <div className="product-description">
+            <h3>Description</h3>
+            <p>{product.description}</p>
+          </div>
+          
+          <button className="add-to-cart-button" disabled={product.stock === 0}>
+            Ajouter au panier
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
