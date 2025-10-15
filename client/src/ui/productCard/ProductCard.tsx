e4fde45950ae5832fa094a2a067101c2aadc7273
fraxi0n <85115114+fraxi0n@users.noreply.github.com>
2025-10-15 15:00:34 +0200

    style product card

--- client/src/ui/productCard/ProductCard.tsx
+++ client/src/ui/productCard/ProductCard.tsx
@@ -1,16 +1,30 @@
 import type { Product } from "../../models/Products";
+import { Button } from "../button/Button";
 import { ImgComp } from "../imgComp/ImgComp";
 import "./ProductCard.css";
+import "../../App.css";
+import { useNavigate } from "react-router-dom";
+
 
 interface ProductCardProps {
   product: Product
-//   variant: "primary" | "secondary";
+  //   variant: "primary" | "secondary";
 }
 
-export const ProductCard = ({product}: ProductCardProps) => {
-  return <div>
-    <ImgComp path={product.picturePath} size={300}/>
-   {product.getName()}
-  {product.price}
+export const ProductCard = ({ product }: ProductCardProps) => {
+
+  const navigate =useNavigate()
+
+  return <div className="product-card">
+    <ImgComp path={product.picturePath} size={300} />
+    <p>
+      {product.getName()}
+    </p>
+    <div className="price-btn">
+      <Button label="voir produit" onClick={()=>navigate("/produit/"+product.id)} />
+      <p>
+        {product.price + "€"}
+      </p>
+    </div>
   </div>
 };
