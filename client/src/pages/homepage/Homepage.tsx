6d492be297ac938504d66f4c5a6b503c884736a3
fraxion <fraxion26@gmail.com>
2025-10-17 17:06:19 +0200

    add link into homepage

--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -1,12 +1,29 @@
 // page d'accueil / homepage
 
+import { Link } from "react-router-dom";
+import { useAnimalsCategoriesFetch } from "../../hooks/useAnimalsCategoriesFetch";
 import { Carousel } from "../../ui/carousel/Carousel";
+import { ImgComp } from "../../ui/imgComp/ImgComp";
 
 export const Homepage = () => {
-    return(
-        <div>
-            <Carousel searchOption={{animalsCategory : 1}} />
+    const categories = useAnimalsCategoriesFetch()
 
-        </div>
+    return (
+        <>
+            <Link to={"/catalogue"}>
+
+                <div >
+                    <h2>  Découvrez nos univers </h2>
+                    {
+                        categories.map(cat => <>
+                            <ImgComp size={500} path={cat.getpicturePath()}></ImgComp>
+                        </>
+                        )
+
+                    }
+                </div>
+            </Link>
+            <Carousel searchOption={{ animalsCategory: 1 }} />
+        </>
     );
 }
--- client/src/ui/footer/Footer.tsx
+++ client/src/ui/footer/Footer.tsx
@@ -3,6 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
     return (
         <div className="footer">
+            <br />
         <img src={ "/pictures/kingTobby.png"} alt="logo" />
             <p className="titreFooter">TobbyKingDom</p>
             <p>10 rue des Lamas Chinois, 20600 BASTIA</p>
